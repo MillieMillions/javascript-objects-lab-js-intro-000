d41aac072ea9d16b09c7b3d77c446d5ea4171da4
Millie <milliealexblack@gmail.com>
2019-06-23 19:06:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
+
 var recipes = {};
-
-function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign({}, object, {[key]: value});
-}
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object;
-}
-
-function deleteFromObjectByKey(object, key) {
-  delete.Object(0, [key])
-  return Object
-}
